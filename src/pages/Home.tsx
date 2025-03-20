d61602e1233c6d0278d21aa8464177d92cc0805b
Rafa Victor <orafavictor@gmail.com>
2025-03-20 06:36:40 -0300

    Feat: Avatars List Component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,33 @@
-import { Header, CardComponent } from '@/components'
+import { AvatarsList, Header, CardComponent } from '@/components'
 import { Container } from '@mui/material'
+import { currencyConverter } from '@/utils'
 
 function Home() {
+  const mockListData = [
+    {
+      avatar: '/dnc-avatar.svg',
+      name: 'Nome Sobrenome 1',
+      subtitle: currencyConverter(1000),
+    },
+    {
+      avatar: '/dnc-avatar.svg',
+      name: 'Nome Sobrenome 2',
+      subtitle: currencyConverter(1000),
+    },
+    {
+      avatar: '/dnc-avatar.svg',
+      name: 'Nome Sobrenome 2',
+      subtitle: currencyConverter(1000),
+    },
+  ]
   return (
     <>
       <Header />
       <Container maxWidth="lg">
         <CardComponent>CARD</CardComponent>
+        <CardComponent>
+          <AvatarsList listData={mockListData} />
+        </CardComponent>
       </Container>
     </>
   )
