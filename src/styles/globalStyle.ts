818378fb78188896b46ab8e4a84576daa7021a5f
Rafa Victor <orafavictor@gmail.com>
2025-05-24 09:10:02 -0300

    Feat: Logout Flow

--- src/styles/globalStyle.ts
+++ src/styles/globalStyle.ts
@@ -1,3 +1,4 @@
+import { pxToRem } from '@/utils'
 import { createGlobalStyle } from 'styled-components'
 import { DefaultTheme } from 'styled-components'
 
@@ -12,5 +13,13 @@ export const GlobalStyle = createGlobalStyle<{ theme?: DefaultTheme }>`
     h1, h2, p, ul, li, figure {
         margin: 0;
         padding: 0;
+    }
+
+    .mb-1 {
+        margin-bottom: ${pxToRem(16)};
+    }
+
+    .mb-2 {
+        margin-bottom: ${pxToRem(32)};
     }
 `
